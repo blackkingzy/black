f400b67323bcbc66cc41984401f9d523aa8d9e57
blackkingzy <1003975792@qq.com>
2020-11-10 21:42:01 +0800

    update

--- lib/util.ts
+++ lib/util.ts
@@ -11,9 +11,9 @@ const load = (folder: string, options: ILoadOptions = {}, app: Black) => {
     router = app.$router;
     const extname = options.extname || ".{js,ts}";
     //sync返回读取的所有文件的路径数组
-    glob.sync(join(folder, `./**/*controller${extname}`))
+    glob.sync(join(folder, `./**/*${extname}`))
         .filter((v) => v.indexOf(".spec") === -1) // 排除测试代码
-        .filter((v) => /controller.ts/i.test(v))
+        .filter((v) => /controller/i.test(v))
         .forEach((item) => require(item));
 };
 
